25c92f22c1d8a6cd991872183a2d7e266e773a7d
M.O Akindayo <matthewakinse@gmail.com>
2020-05-02 09:50:05 +0300

    Fix errors and Add function to expand to current node's neighbor

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@
 
 using std::abs;
 using std::cout;
-using std::fstream;
+using std::ifstream;
 using std::istringstream;
 using std::sort;
 using std::string;
@@ -21,6 +21,9 @@ enum class State
   kClosed,
   kPath
 };
+
+// directional deltas
+const int delta[4][2]{{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
 
 // Function to read and parse string stream
 vector<State> ParseLine(string line)
@@ -44,22 +47,18 @@ vector<State> ParseLine(string line)
 }
 
 // Fill board from file content
-auto ReadFromfile(string path)
+vector<vector<State>> ReadFromfile(string path)
 {
   vector<vector<State>> b{};
-
   //file open and stream
-  fstream my_file;
-  my_file.open(path);
-
+  ifstream my_file(path);
   if (my_file)
   {
     cout << "The stream has been created \n";
     string line;
-    vector<State> srow;
     while (getline(my_file, line))
     {
-      srow = ParseLine(line);
+      vector<State> srow = ParseLine(line);
       b.push_back(srow);
     }
   }
@@ -69,11 +68,9 @@ auto ReadFromfile(string path)
 // Write function to compare the f-value of two nodes here
 bool Compare(const vector<int> n1, const vector<int> n2)
 {
-  int f1 = n1[2] + n1[3];
-  int f2 = n2[2] + n2[3];
-  bool compare = f1 > f2 ? true : false;
-
-  return compare;
+  int f1 = n1[2] + n1[3]; // f1 = g1 + h1
+  int f2 = n2[2] + n2[3]; // f2 = g2 + h2
+  return f1 > f2;
 }
 
 /**
@@ -88,13 +85,7 @@ void CellSort(vector<vector<int>> *v)
 
 int Heuristic(int x1, int y1, int x2, int y2)
 {
-
-  int diffx = abs(x2 - x1);
-  int diffy = abs(y2 - y1);
-
-  int sumdiff = diffx + diffy;
-
-  return sumdiff;
+  return abs(x2 - x1) + abs(y2 - y1);
 }
 
 // TODO: Write CheckValidCell here.
@@ -102,8 +93,8 @@ int Heuristic(int x1, int y1, int x2, int y2)
 bool CheckValidCell(int x, int y, vector<vector<State>> &grid)
 {
   // Check that (x,y) coordinatea pair is on the grid
-  bool x_on_grid = x > 0 && x < grid.size() ? true : false;
-  bool y_on_grid = y > 0 && y < grid[0].size() ? true : false;
+  bool x_on_grid = (x >= 0 && x < grid.size());
+  bool y_on_grid = (y >= 0 && y < grid[0].size());
 
   // Check that the grid at (x,y) is kEmpty
   if (x_on_grid && y_on_grid)
@@ -121,7 +112,46 @@ void AddToOpen(int x, int y, int g, int h, vector<vector<int>> &openlist, vector
   grid[x][y] = State::kClosed;
 }
 
-// write search function
+/** 
+ * Expand current nodes's neighbors and add them to the open list.
+ */
+// TODO: ExpandNeighbors(arguments) {
+
+// TODO: Get current node's data.
+
+// TODO: Loop through current node's potential neighbors.
+
+// TODO: Check that the potential neighbor's x2 and y2 values are on the grid and not closed.
+
+// TODO: Increment g value, compute h value, and add neighbor to open list.
+
+// } TODO: End the function
+
+void ExpandNeighbors(const vector<int> &current, int goal[2], vector<vector<int>> &openlist, vector<vector<State>> &grid)
+{
+  // Get current node's data.
+  int x = current[0];
+  int y = current[1];
+  int g = current[2];
+
+  // Loop through current node's potential neighbors.
+  for (int i = 0; i < 4; i++)
+  {
+    int x2 = x + delta[i][0];
+    int y2 = y + delta[i][1];
+
+    // Check that the potential neighbor's x2 and y2 values are on the grid and not closed.
+    if (CheckValidCell(x2, y2, grid))
+    {
+      // Increment g value and add neighbor to open list.
+      int g2 = g + 1;
+      int h2 = Heuristic(x2, y2, goal[0], goal[1]);
+      AddToOpen(x2, y2, g2, h2, openlist, grid);
+    }
+  }
+}
+
+// write A* search algorithm in function
 vector<vector<State>> Search(vector<vector<State>> grid, int init[2], int goal[2])
 {
   // TODO: while open vector is non empty {
@@ -144,8 +174,8 @@ vector<vector<State>> Search(vector<vector<State>> grid, int init[2], int goal[2
   int y = init[1];
   int g = 0;
   int h = Heuristic(x, y, goal[0], goal[1]);
-
   AddToOpen(x, y, g, h, open, grid);
+
   while (open.size() > 0)
   {
     // Get the next node
@@ -164,10 +194,10 @@ vector<vector<State>> Search(vector<vector<State>> grid, int init[2], int goal[2
 
     // If we're not done, expand search to current node's neighbors.
     // ExpandNeighbors
+    ExpandNeighbors(current, goal, open, grid);
   }
 
   // We've run out of new nodes to explore and haven't found a path.
-
   cout << "No path found!"
        << "\n";
   return std::vector<vector<State>>{};
